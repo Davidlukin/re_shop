a84475d4203e5999c7113a27e52fab168b176c2d
David Lukin <david.lukin.99@bk.ru>
2023-10-16 23:06:53 +0300

    16.10.2023

--- re_shop/product/models.py
+++ re_shop/product/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=150)
     price = models.IntegerField()
     description = models.CharField(max_length=700)
+    image = models.ImageField(blank=True,upload_to='images')
 
     def __str__(self):
         return self.name
